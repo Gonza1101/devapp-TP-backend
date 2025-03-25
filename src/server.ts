377360bfb8b7f6a73a0479142f7800aad872fda0
Gonza1101 <gonzalo.villalba1987@gmail>
2025-03-25 20:08:55 -0300

    Creacion de Backend

---
+++ src/server.ts
@@ -0,0 +1,29 @@
+/* eslint-disable prettier/prettier */
+// Importamos nuestras dependencias
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import helmet from 'helmet';
+import process from 'process';
+
+// Creamos nuestra app express
+const app = express();
+// Leemos el puerto de las variables de entorno, si no está, usamos uno por default
+const port = process.env.PORT || 9000;
+
+// Configuramos los plugins
+// Más adelante intentaremos entender mejor cómo funcionan estos plugins
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+
+// Mis endpoints van acá
+app.get('/',(req,res)=>{
+    res.json('ta ta ta ta ta - Feliz Navidad, inmundo animal - ta ta ta ta ta -  Y feliz Año Nuevo')
+})
+// ...
+
+// Levantamos el servidor en el puerto que configuramos
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+});
