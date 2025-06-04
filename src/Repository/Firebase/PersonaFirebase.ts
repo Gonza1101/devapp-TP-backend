479948035ac7825f39ba979f1c6bee64f4975144
Gonza1101 <gonzalo.villalba1987@gmail>
2025-06-03 21:50:14 -0300

    Ajustes

--- src/Repository/Firebase/PersonaFirebase.ts
+++ src/Repository/Firebase/PersonaFirebase.ts
@@ -1,7 +1,7 @@
 import { Persona } from '../../Model/Persona';
 import { Auto } from '../../Model/Auto';
 import { firebasedb } from '../../DataBase/ConnectFirebase';
-import { collection, getDoc, getDocs } from 'firebase/firestore/lite';
+import { collection, getDocs } from 'firebase/firestore/lite';
 
 export const PersonaMongodb = {
     listadoPersona: async (): Promise<Persona[]> => {
@@ -11,40 +11,40 @@ export const PersonaMongodb = {
         return personas;
     },
     personaConDni: async (dni: string): Promise<Persona | null> => {
-        const db = await ConnectMongodb();
+        // const db = await ConnectMongodb();
         const persona = await db.collection<Persona>('persona').findOne({ dni: { $eq: dni } });
-        closeMongodb();
+        // closeMongodb();
         return persona;
     },
     personaConId: async (id: string): Promise<Persona | null> => {
-        const db = await ConnectMongodb();
+        // const db = await ConnectMongodb();
         const persona = await db.collection<Persona>('persona').findOne({ _id: { $eq: id } });
-        closeMongodb();
+        // closeMongodb();
         return persona;
     },
 
     agregarPersona: async (persona: Persona) => {
-        const db = await ConnectMongodb();
-        await db.collection<Persona>('persona').insertOne(persona);
-        closeMongodb();
+        // const db = await ConnectMongodb();
+        // await db.collection<Persona>('persona').insertOne(persona);
+        // closeMongodb();
     },
     agregarAuto: async (idPersona: string, auto: Auto) => {
-        const db = await ConnectMongodb();
-        await db.collection<Persona>('persona').updateOne({ _id: { $eq: idPersona } }, { $push: { autos: auto } });
-        await db.collection<Auto>('auto').insertOne(auto);
-        closeMongodb();
+        // const db = await ConnectMongodb();
+        // await db.collection<Persona>('persona').updateOne({ _id: { $eq: idPersona } }, { $push: { autos: auto } });
+        // await db.collection<Auto>('auto').insertOne(auto);
+        // closeMongodb();
     },
     eliminaPersona: async (idPersona: string) => {
-        const db = await ConnectMongodb();
-        await db.collection<Persona>('persona').deleteOne({ _id: { $eq: idPersona } });
-        closeMongodb();
+        // const db = await ConnectMongodb();
+        // await db.collection<Persona>('persona').deleteOne({ _id: { $eq: idPersona } });
+        // closeMongodb();
     },
     eliminarAuto: async (idPersona: string, idAuto: string) => {
-        const db = await ConnectMongodb();
-        db.collection<Persona>('persona').updateOne(
-            { _id: { $eq: idPersona } },
-            { $pull: { autos: { _id: { $eq: idAuto } } } }
-        );
-        closeMongodb();
+        // const db = await ConnectMongodb();
+        // db.collection<Persona>('persona').updateOne(
+        // { _id: { $eq: idPersona } },
+        // { $pull: { autos: { _id: { $eq: idAuto } } } }
+        // );
+        // closeMongodb();
     }
 };
